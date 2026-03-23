0db37b635f0f594126d51570134ac45f52460d0a
Dysky2 <dawid4799@gmail.com>
2026-03-23 18:16:49 +0100

    Initial commit

---
+++ src/main/java/net/edu/modulartask/ModularTaskApplication.java
@@ -0,0 +1,13 @@
+package net.edu.modulartask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModularTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/edu/modulartask/ModularTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package net.edu.modulartask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
