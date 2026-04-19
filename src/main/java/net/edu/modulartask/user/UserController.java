f4f9a5b1eea853647a89402bc0d04b28ae2e7249
Dysky2 <dawid4799@gmail.com>
2026-04-19 20:17:46 +0200

    MOD-20: Reduce redundant parts of addresses

--- src/main/java/net/edu/modulartask/user/UserController.java
+++ src/main/java/net/edu/modulartask/user/UserController.java
@@ -9,22 +9,23 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/users")
 @CrossOrigin("http://localhost:5173")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/api/users/all")
+    @GetMapping("/all")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public User getUserById(@PathVariable UUID id) {
         return userService.getUserById(id);
     }
 
-    @PostMapping("/api/users/create")
+    @PostMapping("/create")
     public ResponseEntity<User> createUser(@Valid @RequestBody CreateUserDTO createUserDTO) {
         User createdUser = userService.createUser(createUserDTO);
         return ResponseEntity.ok(createdUser);
