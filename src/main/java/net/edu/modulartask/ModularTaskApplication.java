0a5c31158d5422f4d70478795af0683c7915ca44
Dysky2 <dawid4799@gmail.com>
2026-05-08 19:22:15 +0200

    MOD-34: Add Scheduling to application

--- src/main/java/net/edu/modulartask/ModularTaskApplication.java
+++ src/main/java/net/edu/modulartask/ModularTaskApplication.java
@@ -2,8 +2,10 @@ package net.edu.modulartask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ModularTaskApplication {
 
     public static void main(String[] args) {
