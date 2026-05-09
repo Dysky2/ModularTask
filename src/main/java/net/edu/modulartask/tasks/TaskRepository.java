b2a4086e53e7155e346c184b3930574f5c5c8c12
Dysky2 <dawid4799@gmail.com>
2026-05-09 16:20:56 +0200

    MOD-34: Add new notifications for task taken, rejected and accepts

--- src/main/java/net/edu/modulartask/tasks/TaskRepository.java
+++ src/main/java/net/edu/modulartask/tasks/TaskRepository.java
@@ -2,6 +2,8 @@ package net.edu.modulartask.tasks;
 
 import net.edu.modulartask.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,4 +23,26 @@ public interface TaskRepository extends JpaRepository<Task, UUID> {
     List<Task> findAllByAssignees(Set<User> assignees);
 
     List<Task> findByAssigneesContaining(User user);
+
+    @Query(value = """
+    WITH RECURSIVE TasksRecursive AS (
+        SELECT id, title, description, status, creator_id, parent_task_id, deadline, created_at, task_limit
+        FROM tasks
+        WHERE creator_id = :creatorId
+
+        UNION ALL
+
+        SELECT t.id, t.title, t.description, t.status, t.creator_id, t.parent_task_id, t.deadline, t.created_at, t.task_limit
+        FROM tasks t
+        JOIN TasksRecursive tr ON t.parent_task_id = tr.id
+    )
+    SELECT DISTINCT id, title, description, status, creator_id, parent_task_id, deadline, created_at, task_limit
+    FROM TasksRecursive
+    WHERE status = CAST(:status AS text)
+    """, nativeQuery = true)
+    List<Task> findAllTasksByCreatorIdAndStatus(
+            @Param("creatorId") UUID creatorID,
+            @Param("status") String status
+    );
+
 }
